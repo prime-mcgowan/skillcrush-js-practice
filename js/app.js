b6e751936c88b2262db61d984ce20af8a42778cd
Laura McGowan <prime.lmcg@gmail.com>
2024-02-26 12:26:08 -0600

    initial commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+let firstName = "Laura";
+let favoriteSnack = "Chips";
+
+console.log("Hi, my name is " + firstName);
+console.log(
+  "Hi, my name is " + firstName + " and I my favorite snack is " + favoriteSnack
+);
