d8cff10218f87e4ebc1378c37da9147c60a072a3
Laura McGowan <prime.lmcg@gmail.com>
2024-02-26 13:18:04 -0600

    check calculation exercise

--- js/app.js
+++ js/app.js
@@ -20,9 +20,27 @@ let sold = 12;
 
 console.log(`The prize is a ${prize}!! Only ${tickets - sold} tickets remain.`);
 
+// * Converting Celsius to Fahrenheit ************************************************************
 let tempCelsius = Number(10);
 let toFahrenheit = tempCelsius * 1.8 + 32;
 
 console.log(
   `Outside my friend's house it's ${tempCelsius}, which is ${toFahrenheit}.`
 );
+
+// * Split the Check Calculator ************************************************************
+let mealCost = 25.99;
+let tip = 0.18;
+let tipAmount = mealCost * tip;
+let total = mealCost + tipAmount;
+let numOfPeople = 2;
+
+console.log(`Your meal cost is $${mealCost}, and with tip, it comes to $${total.toFixed(
+  2
+)}.
+Your share is $${(total / numOfPeople).toFixed(2)}. Hope it was delicious!`);
+
+// prompt use example: let moneyRaised = Number(prompt("How much money did we raise?"));
+
+let mealCostQeustion = Number(prompt("Cost of meal (ex. 25.99)"));
+console.log(mealCostQeustion);
