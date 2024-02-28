f6da76f0e8d2a7b22dcd9f6055d157a78e95c615
Laura McGowan <prime.lmcg@gmail.com>
2024-02-28 17:42:43 -0600

    event listeners & handlers practice

--- js/app.js
+++ js/app.js
@@ -214,3 +214,53 @@ if (time < 12) {
 // openHours.style.border = "1px solid #d45bb9";
 // openHours.style.padding = "1em";
 // console.log(openHours);
+
+// * DOM Events Event Listeners & Handlers ************************************************************
+// let button = document.querySelector("button");
+// let special = document.querySelector("special");
+
+// button.addEventListener("click", function () {
+//   special.innerText = "Fuji apples on sale!!";
+//   special.style.fontSize = "3em";
+//   button.classList.add("deemphasize-button");
+// });
+
+let title = document.querySelector("h1");
+title.addEventListener("mouseover", function () {
+  title.innerText = "Let's Party!!";
+  title.style.color = "maroon";
+});
+
+let darkModeButton = document.querySelector(".dark-mode");
+let body = document.querySelector("body");
+
+// darkModeButton.addEventListener("click", function () {
+//   body.classList.add("dark-palette");
+// });
+
+// let lightModeButton = document.querySelector(".light-mode");
+
+// lightModeButton.addEventListener("click", function () {
+//   body.classList.remove("dark-palette");
+// });
+
+// darkModeButton = document.querySelector(".dark-mode");
+let theme = document.querySelector("body");
+
+// darkModeButton.addEventListener("click", function () {
+//   if (theme.classList.contains("dark-palette")) {
+//     theme.classList.remove("dark-palette");
+//   } else {
+//     theme.classList.add("dark-palette");
+//   }
+// });
+
+darkModeButton.addEventListener("click", function () {
+  if (theme.classList.contains("dark-palette")) {
+    theme.classList.remove("dark-palette");
+    darkModeButton.innerText = "Dark Mode";
+  } else {
+    theme.classList.add("dark-palette");
+    darkModeButton.innerText = "Light Mode";
+  }
+});
