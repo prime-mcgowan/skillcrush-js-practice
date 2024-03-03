344c2899086bb73465434dc3312e1b982a0638a4
Laura McGowan <prime.lmcg@gmail.com>
2024-03-03 10:13:32 -0600

    click kitty event - remove & add classes

--- js/app.js
+++ js/app.js
@@ -273,5 +273,13 @@ let cat = document.querySelector(".cat");
 console.log(cat);
 
 button.addEventListener("click", function () {
-  cat.classList.add("show");
+  if (cat.classList.contains("show")) {
+    cat.classList.remove("show");
+    button.innerText = "Wait, come back!!";
+    button.classList.add("disappear");
+  } else {
+    cat.classList.add("show");
+    button.innerText = "Shoo, cat!!";
+    button.classList.remove("disappear");
+  }
 });
