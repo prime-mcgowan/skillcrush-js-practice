f543d486d1941b3de10b0abe3699a8b752b65fd0
Laura McGowan <prime.lmcg@gmail.com>
2024-03-10 12:56:37 -0500

    methods = created new properties, added values and conditional statements, called methods

--- js/app.js
+++ js/app.js
@@ -1083,12 +1083,30 @@ console.log(paperclips);
 // * Properties and Dot Notation ****************************************
 const topicElement = document.querySelector(".topic");
 const countElement = document.querySelector(".count");
+const body = document.querySelector("body");
+const success = document.querySelector(".success");
+const dance = document.querySelector(".dance");
 
 const learning = {
   topic: "JS",
   learningGoals: ["html", "css", "js", "react"],
   category: "Front End Developement",
   topicImportance: "high",
+  hoursThisWeek: 2,
+  weeklyHourGoal: 7,
+  achievedStudyGoal: false,
+  addStudyTime: function (hours) {
+    this.hoursThisWeek += hours;
+    if (this.hoursThisWeek >= this.weeklyHourGoal) {
+      this.achievedStudyGoal = true;
+      this.celebrate();
+    }
+  },
+  celebrate: function () {
+    body.classList.add("celebrate");
+    success.classList.remove("hide");
+    dance.classList.remove("hide");
+  },
 };
 
 console.log(learning);
@@ -1103,3 +1121,5 @@ topicElement.classList.remove("hide");
 
 countElement.innerText = `I have ${learning.learningGoals.length} learning goals.`;
 countElement.classList.remove("hide");
+
+learning.addStudyTime(10);
