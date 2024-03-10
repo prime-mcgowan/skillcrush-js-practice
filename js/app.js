d5738a72ddefac51aa85d830a926112ebbbdf884
Laura McGowan <prime.lmcg@gmail.com>
2024-03-10 17:51:57 -0500

    display pet objects practice

--- js/app.js
+++ js/app.js
@@ -1299,9 +1299,36 @@ const baxter = createPet("Baxter", "hamster");
 const cleo = createPet("Cleo", "rat");
 const francine = createPet("Francine", "turtle");
 
-console.log(sora, clover, baxter, cleo, francine);
+// console.log(sora, clover, baxter, cleo, francine);
 
-clover.sleep();
-baxter.play();
+// clover.sleep();
+// baxter.play();
 
-console.log(clover, baxter);
+// console.log(clover, baxter);
+
+clover.isTired = 8;
+francine.isTired = 9;
+
+// console.log(sora, clover, baxter, cleo, francine);
+
+const allPets = [sora, clover, baxter, cleo, francine];
+
+// console.log(allPets);
+
+const showPets = function (petArray) {
+  pets.innerHTML = "";
+
+  for (let pet of petArray) {
+    let status = "Ready to play!!";
+    if (pet.isTired >= 7) {
+      status = "sleeping";
+    }
+    const li = document.createElement("li");
+    li.innerHTML = `<span class="pet-name"> ${pet.name}</span>, the ${pet.species}, is ${status}`;
+    pets.append(li);
+  }
+};
+
+statusButton.addEventListener("click", function () {
+  showPets(allPets);
+});
